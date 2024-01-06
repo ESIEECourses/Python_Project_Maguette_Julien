87a73eead3cc6145f10cf36692fbbc3178fb5c7f
Maguette MADIODIOU DIAGNE <maguette.madiodioudiagne@grtgaz.com>
2024-01-06 19:33:54 +0100

    Ajout de lhistogramme dans le dashboard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# main.py
+from dashboard import create_dash_application
 
-
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    app = create_dash_application()
+    app.run_server(debug=True)
